fef13c4afa7c8c34e1f87fdf0bdc37d2d1aa16ce
Deytraaa <satriacahya162@gmail.com>
2025-05-12 22:07:00 +0700

    ,,

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = (e) => {
     e.preventDefault();
     // Belum ada logika autentikasi, langsung pindah halaman
-    navigate('/data-produk');
+    navigate('/dashboard');
   };
 
   return (
